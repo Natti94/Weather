04630148b41ce8a5efa43f8123abe627954cb1a5
Natti94 <natnael.berhane.sv@gmail.com>
2025-05-07 21:46:33 +0200

    update

--- weather-app/vite.config.js
+++ weather-app/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Weather_Leaflet_OpenWeather/",
   plugins: [react()],
-base: "/Weather_Leaflet_OpenWeather"
-})
+});
